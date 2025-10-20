13f1d6dff2b01618ca20645dc435828a81d09981
MasterWolf2077 <damonmiche8@gmail.com>
2025-10-20 14:06:53 +0200

    commit

---
+++ Tasks/Conditional/app.js
@@ -0,0 +1,39 @@
+//Task 1
+let number = prompt("Enter a random number");
+if (number > 90 && number < 110) {
+    alert("Bingo!");
+} else {
+    alert("Miss");
+}
+
+//Task 2
+let num = prompt("Enter a random number");
+let message = (number > 90 && number < 110) ? "Bingo!": "Miss";
+alert(message);
+
+//Task 3
+let firstNumber = Number(prompt("Enter first number"));
+let secondNumber = Number(prompt("Enter second number"));
+let operand = prompt("Enter operand (+, -, * or /)");
+let result;
+
+if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+    switch (operand) {
+        case "+":
+             result = firstNumber + secondNumber;
+             break;
+        case "-":
+            result = firstNumber - secondNumber;
+            break;
+        case "*":
+            result = firstNumber * secondNumber;
+            break;
+        case "/":
+            result = firstNumber / secondNumber;
+            break;
+        default: "Error: unknown operand";
+    }
+} else {
+    result = "Error: at least one of the entered values is not a number";
+}
+alert(result);
