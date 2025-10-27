203151d8fc6877c8865c39c7aa4232db59a2eef6
MasterWolf2077 <damonmiche8@gmail.com>
2025-10-27 15:16:42 +0200

    commit

---
+++ Exercises/Shadowing/app.js
@@ -0,0 +1,9 @@
+function subtract(a, b) {
+    return a - b;
+}
+console.log(subtract(10, 4)); // Output: 6
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 5)); // Output: 15
